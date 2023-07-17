37e3f66aab66c5a26ac92b8e41d240a7f835edb6
Aleksandr Zavialov <aleksandr.zavialov@grse.de>
2023-07-17 20:02:20 +0300

    Test cases automated - need to connect allure

--- python_gismeteo_mobile/tests/conftest.py
+++ python_gismeteo_mobile/tests/conftest.py
@@ -1,13 +1,16 @@
 import allure
 import pytest
 from selene.support.shared import browser
+from selene import be
 from appium import webdriver
+from appium.webdriver.common.appiumby import AppiumBy
 
 import config
 
 
 @pytest.fixture(scope='function', autouse=True)
 def driver_management(request):
+
     browser.config.timeout = config.settings.timeout
     with allure.step('set up app session'):
         browser.config.driver = webdriver.Remote(
@@ -15,5 +18,20 @@ def driver_management(request):
         )
 
     yield
+    return_to_main()
+    return_english_from_russian()
 
+
+def return_to_main():
+    while browser.element((AppiumBy.ID, 'ru.gismeteo.gismeteo:id/action_search')).matching(be.not_.visible):
+        browser.driver.back()
+
+
+def return_english_from_russian():
+    if browser.element((AppiumBy.XPATH, '//*[contains(@text,"ПОБЛИЗОСТИ")]')).matching(be.visible):
+        browser.element((AppiumBy.CSS_SELECTOR, '.android.widget.ImageView')).click()
+        browser.element((AppiumBy.XPATH, '//android.widget.LinearLayout[3]')).click()
+        browser.element((AppiumBy.XPATH, '//*[contains(@text,"Язык")]')).click()
+        browser.element((AppiumBy.XPATH, '//*[contains(@text,"English")]')).click()
+        return_to_main()
 
